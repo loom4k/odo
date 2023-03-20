87c78e66b31950d26b54c7e971049220876969a4
loom4k <contact@loom4k.me>
2023-03-20 19:24:59 -0400

    Test

--- main.py
+++ main.py
@@ -21,16 +21,20 @@ def select_input(widget):
     selected_input = widget
 
 for i in range(len(label_names)):
-    label = tk.Label(left_frame, text=label_names[i], font=("Arial", 14), padx=5, pady=5)
+    label = tk.Label(left_frame, text=label_names[i], font=("Arial", 18), padx=5, pady=5)
     label.grid(row=i, column=0, sticky="w")
-    text_input = tk.Entry(left_frame, font=("Arial", 14))
+    text_input = tk.Entry(left_frame, font=("Arial", 18))
     text_input.grid(row=i, column=1, padx=5, pady=5)
     input_boxes.append(text_input)
     text_input.bind("<Button-1>", lambda event, widget=text_input: select_input(widget))
 
 # Create the submit button
-submit_button = tk.Button(left_frame, text="Submit", font=("Arial", 14), command=lambda: submit_inputs())
-submit_button.grid(row=len(label_names)+1, column=1, pady=10)
+submit_button = tk.Button(left_frame, text="Submit", font=("Arial", 18), command=lambda: submit_inputs())
+submit_button.grid(row=len(label_names)+2, column=0, pady=10, padx=35)
+
+# Create an exit button
+exit_button = tk.Button(left_frame, text="Exit", font=("Arial", 18), command=root.quit)
+exit_button.grid(row=len(label_names)+2, column=1, pady=10)
 
 # Create the right frame to hold the number pad and submit button
 right_frame = tk.Frame(root, bg="white", width=300, height=root.winfo_screenheight())
@@ -44,23 +48,23 @@ def button_click(number):
 
 # Create a grid of buttons to form the number pad
 for i in range(1, 10):
-    button = tk.Button(right_frame, text=str(i), font=("Arial", 24), command=lambda num=i: button_click(num))
+    button = tk.Button(right_frame, text=str(i), font=("Arial", 28), command=lambda num=i: button_click(num))
     button.grid(row=(i-1)//3, column=(i-1)%3, padx=10, pady=10, sticky="nsew")
 
 # Create a dot button
-button_dot = tk.Button(right_frame, text=".", font=("Arial", 24), command=lambda: button_click("."))
+button_dot = tk.Button(right_frame, text=".", font=("Arial", 28), command=lambda: button_click("."))
 button_dot.grid(row=3, column=0, padx=10, pady=10, sticky="nsew")
 
 # Create a button for zero
-button_zero = tk.Button(right_frame, text="0", font=("Arial", 24), command=lambda: button_click(0))
+button_zero = tk.Button(right_frame, text="0", font=("Arial", 28), command=lambda: button_click(0))
 button_zero.grid(row=3, column=1, padx=10, pady=10, sticky="nsew")
 
 # Create a delete button
-button_delete = tk.Button(right_frame, text="←", font=("Arial", 24), command=lambda: [widget.delete(len(widget.get())-1) for widget in input_boxes if widget==selected_input])
+button_delete = tk.Button(right_frame, text="←", font=("Arial", 28), command=lambda: [widget.delete(len(widget.get())-1) for widget in input_boxes if widget==selected_input])
 button_delete.grid(row=3, column=2, padx=10, pady=10, sticky="nsew")
 
 # Create a button to clear all the text inputs
-button_clear = tk.Button(right_frame, text="Clear", font=("Arial", 14), command=lambda: [widget.delete(0, tk.END) for widget in input_boxes])
+button_clear = tk.Button(right_frame, text="Clear", font=("Arial", 18), command=lambda: [widget.delete(0, tk.END) for widget in input_boxes])
 button_clear.grid(row=4, column=1, pady=10)
 
 def submit_inputs():
