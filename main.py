fc3d2d074a8154f5df3763e4c0c1adc86dcdb0a0
loom4k <contact@loom4k.me>
2023-03-22 20:10:12 -0400

    Add vertical option

--- main.py
+++ main.py
@@ -6,8 +6,11 @@ root = tk.Tk()
 root.attributes('-fullscreen', True)
 
 # Create a frame for the top section (white)
-top_frame = tk.Frame(root, bg='white')
-top_frame.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
+top_frame = tk.Frame(root, bg='white', height=root.winfo_screenheight()//2, )
+top_frame.pack(side=tk.TOP, fill=tk.BOTH)
+
+margin_frame = tk.Frame(root, bg='white', height=20)
+margin_frame.pack(side=tk.TOP, fill=tk.BOTH)
 
 # Create a frame for the bottom section (white)
 bottom_frame = tk.Frame(root, bg='white')
@@ -56,5 +59,32 @@ for i in range(len(label_names)):
     text_input.grid(row=i, column=1, padx=5, pady=5)
     input_boxes.append(text_input)
     text_input.bind("<Button-1>", lambda event, widget=text_input: select_input(widget))
+
+# Create the buttons for the number pad
+button_list = [
+    '7', '8', '9',
+    '4', '5', '6',
+    '1', '2', '3',
+    '0', '.', 'C'
+]
+
+# Create and place the buttons in the bottom frame
+for i in range(len(button_list)):
+    button = tk.Button(bottom_frame, text=button_list[i], width=5, height=2)
+    button.grid(row=i//3, column=i%3)
+
+# Create the submit button
+submit_button = tk.Button(top_frame, text="Submit", font=("Arial", 18), command=lambda: submit_inputs())
+submit_button.grid(row=len(label_names)+2, column=1, pady=10, padx=35)
+
+# Create an exit button
+exit_button = tk.Button(top_frame, text="Exit", font=("Arial", 18), command=root.quit)
+exit_button.grid(row=len(label_names)+2, column=0, pady=10)
+
+
+def submit_inputs():
+    # Get the values from the text input widgets and print them
+    values = [widget.get() for widget in input_boxes]
+    print(values)
 
 root.mainloop()
