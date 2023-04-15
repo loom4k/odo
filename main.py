a5a79709ea7cf53dd6ff8cc56c71605ce1a213fb
loom4k <contact@loom4k.me>
2023-04-14 21:05:48 -0400

    Make window go full screen?

--- main.py
+++ main.py
@@ -7,12 +7,23 @@ cap = cv2.VideoCapture(0)
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
 
+cv2.namedWindow('frame', cv2.WINDOW_NORMAL)
+
 while True:
     # Capture a frame
     ret, frame = cap.read()
 
     # Flip the frame horizontally
     frame = cv2.flip(frame, 1)
+
+    # Draw two vertical lines in the middle of the screen
+    y1 = frame.shape[0] // 2 - 50
+    y2 = frame.shape[0] // 2 + 50
+    line_width = frame.shape[1] // 3
+    x1 = (frame.shape[1] - line_width) // 2
+    x2 = x1 + line_width
+    cv2.line(frame, (x1, y1), (x2, y1), (0, 255, 0), 2)
+    cv2.line(frame, (x1, y2), (x2, y2), (0, 255, 0), 2)
 
     # Display the frame
     cv2.imshow('frame', frame)
